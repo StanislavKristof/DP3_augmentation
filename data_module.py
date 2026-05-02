19a3e86abaca3e92d15a6667920842d8c41469d4
StanislavKristof <xkristof@stuba.sk>
2026-05-02 07:29:03 +0200

    the rest of augmentations

--- data_module.py
+++ data_module.py
@@ -91,19 +91,20 @@ class CroppedSVHNDataset(Dataset):
         if self.split == "test":
             self.transform = A.Compose([A.NoOp(p=1)])
         elif self.split == "train":
-            self.transform = A.Compose([
-                A.Affine(
-                    rotate=(-10,10),
-                    p=0.3
-                ),
-                A.ColorJitter(
-                    brightness=0.2, 
-                    contrast=0.2, 
-                    saturation=0.2, 
-                    hue=0.05, 
-                    p=0.4
-                )
-            ])
+            # self.transform = A.Compose([
+            #     A.Affine(
+            #         rotate=(-10,10),
+            #         p=0.3
+            #     ),
+            #     A.ColorJitter(
+            #         brightness=0.2, 
+            #         contrast=0.2, 
+            #         saturation=0.2, 
+            #         hue=0.05, 
+            #         p=0.4
+            #     )
+            # ])
+            self.transform = A.Compose([A.NoOp(p=1)])
             
         self.length = self.data["X"].shape[-1]
 
@@ -188,20 +189,21 @@ class CIFAR10Dataset(Dataset):
         if self.split == "test":
             self.transform = A.Compose([A.NoOp(p=1)])
         elif self.split == "train":
-            self.transform = A.Compose([
-                A.HorizontalFlip(p=0.5),
-                A.Affine(
-                    rotate=(-10,10),
-                    p=0.3
-                ),
-                A.ColorJitter(
-                    brightness=0.1, 
-                    contrast=0.1, 
-                    saturation=0.1, 
-                    hue=0.05, 
-                    p=0.3
-                )
-            ])
+            # self.transform = A.Compose([
+            #     A.HorizontalFlip(p=0.5),
+            #     A.Affine(
+            #         rotate=(-10,10),
+            #         p=0.3
+            #     ),
+            #     A.ColorJitter(
+            #         brightness=0.1, 
+            #         contrast=0.1, 
+            #         saturation=0.1, 
+            #         hue=0.05, 
+            #         p=0.3
+            #     )
+            # ])
+            self.transform = A.Compose([A.NoOp(p=1)])
 
 
         self.data = [
@@ -304,20 +306,21 @@ class CIFAR100Dataset(Dataset):
 
         split = dataset_path.name
         if split == "train":
-            self.transform = A.Compose([
-                A.HorizontalFlip(p=0.4),
-                A.Affine(
-                    rotate=(-10,10),
-                    p=0.3
-                ),
-                A.ColorJitter(
-                    brightness=0.1, 
-                    contrast=0.1, 
-                    saturation=0.1, 
-                    hue=0.05, 
-                    p=0.2
-                )
-            ])
+            # self.transform = A.Compose([
+            #     A.HorizontalFlip(p=0.4),
+            #     A.Affine(
+            #         rotate=(-10,10),
+            #         p=0.3
+            #     ),
+            #     A.ColorJitter(
+            #         brightness=0.1, 
+            #         contrast=0.1, 
+            #         saturation=0.1, 
+            #         hue=0.05, 
+            #         p=0.2
+            #     )
+            # ])
+            self.transform = A.Compose([A.NoOp(p=1)])
         elif split == "test":
             self.transform = A.Compose([A.NoOp(p=1)])
 
@@ -357,104 +360,6 @@ class CIFAR100Dataset(Dataset):
         return image, fine_label, idx
 
 
-class StanfordCarsDataset(Dataset):
-    """Represents the Stanford Cars Dataset.
-
-    The Stanford Cars dataset contains 16185 images of 196 classes of cars. The
-    class label usually consists of the make, model and year. Images have
-    various sizes, some are RGB, some are greyscale.
-
-    The dataset is split like this:
-        train = 8144 images 
-        test = 8041 images
-
-    Source: https://huggingface.co/datasets/tanganke/stanford_cars
-
-    Downloaded at: 2025/02/16/13/24
-    """
-
-    def __init__(
-            self,
-            dataset_path: str,
-            ) -> None:
-        """Construct a StanfordCarsDataset object.
-
-        Constructs a StanfordCarsDataset object using the specified dataset
-        path.
-
-        Parameters:
-            dataset_path (str): path to the datafile
-        """
-        self.dataset_path = Path(dataset_path)
-        split = self.dataset_path.name
-        dataset = load_dataset("tanganke/stanford_cars")
-        self.dataset = dataset[split]
-        self.length = len(self.dataset)
-        if split == "train":
-            self.transform = A.Compose([
-                A.Sharpen(p=.2),
-                #A.ColorJitter(p=.1, hue=(-.03, .03)), # hue=0
-                #A.Emboss(p=.1),
-                A.HueSaturationValue(p=.1, hue_shift_limit=(-10, 10)),
-                A.ISONoise(p=.1),
-                A.ImageCompression(p=.1, quality_range=(90, 95)),
-                # A.InvertImg(p=.05),
-                #A.RGBShift(p=.15),
-                A.OneOf([
-                    A.Solarize(p=.5, threshold_range=(0.8, 0.8)),
-                    A.Solarize(p=.5, threshold_range=(0.9, 0.9)),
-                ], p=.1),
-
-                #A.Superpixels(p=.1, max_size=12, n_segments=(2, 8)),
-
-                A.OneOf([
-                    A.ToGray(p=.6),
-                    A.ToSepia(p=.4),
-                ], p=.1),
-
-                A.CoarseDropout(p=.1),
-                # A.HorizontalFlip(p=.1),
-                # A.Morphological(p=.15, operation="erosion", scale=3),
-                # A.Morphological(p=.15, operation="dilation", scale=3),
-                A.PixelDropout(p=.1, per_channel=True),
-                A.RandomRotate90(p=.1),
-                # A.RandomSizedCrop(p=.1, min_max_height=(28, 28), size=(32, 32)),
-                A.Rotate(p=.1),
-                A.Transpose(p=.1),
-            ])
-        elif split == "test":
-            self.transform = A.Compose(A.NoOp(p=1))
-
-    def __len__(self) -> int:
-        """Obtain length of the data.
-
-        Returns:
-            int: quantity of the data
-        """
-        return self.length
-
-    def __getitem__(
-            self,
-            idx: int
-            ) -> tuple[np.ndarray, int]:
-        """Get item of the dataset.
-
-        Parameters:
-            idx (int): index of the datapoint in the dataset
-
-        Returns:
-            a tuple containing an image and label
-        """
-        image_path = get_data_file_path(idx, 4, ".png")
-        image = cv.imread(self.dataset_path / image_path)
-        image = self.transform(image=image)["image"]
-        image = image / 255
-        image = image.reshape(3, 256, 256)
-
-        label = self.dataset[idx]["label"]
-
-        return image, label
-
 class Covid19Dataset(Dataset):
     """Represents the Covid-19 Dataset.
 
@@ -485,14 +390,15 @@ class Covid19Dataset(Dataset):
         self.dataset_path = Path(dataset_path)
         self.split = self.dataset_path.name
         if self.split == "train":
-            self.transform = A.Compose([
-                A.HorizontalFlip(p=0.3),
-                A.Affine(
-                    rotate=(-5,5),
-                    p=0.3
-                ),
-                A.GaussNoise(p=0.3, std_range=(0.05, 0.2))
-            ])
+            # self.transform = A.Compose([
+            #     A.HorizontalFlip(p=0.3),
+            #     A.Affine(
+            #         rotate=(-5,5),
+            #         p=0.3
+            #     ),
+            #     A.GaussNoise(p=0.3, std_range=(0.05, 0.2))
+            # ])
+            self.transform = A.Compose([A.NoOp(p=1)])
         elif self.split == "test":
             self.transform = A.Compose([A.NoOp(p=1)])
         self.anno_file = pd.read_csv(self.dataset_path / "anno.csv")
